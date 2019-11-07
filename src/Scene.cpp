c7f81da4d9cd41c4b2898ff03f5e3ce6d6c9c4db
zane1999 <zane1999@zane1999deAir.wlan.jacobs-university.de>
2019-11-07 21:08:56 +0100

    homework4

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -17,8 +17,12 @@ void CScene::ParseOBJ(const std::string& fileName)
 	if (file.is_open()) {
 		std::cout << "Parsing OBJFile : " << fileName << std::endl;
 
-		std::shared_ptr<IShader> pShader = std::make_shared<CShaderEyelight>(RGB(1, 0.5f, 0));
-		std::vector<Vec3f> vVertexes;
+		//std::shared_ptr<IShader> pShader = std::make_shared<CShaderEyelight>(RGB(1, 0.5f, 0));
+		//std::vector<Vec3f> vVertexes;
+		std::shared_ptr<IShader> pShader = std::make_shared<CShaderEyelightTextured>(RGB(1, 1, 1), "../../../../data/cb.bmp");
+		std::vector<Vec3f> vVertices;
+		std::vector<Vec3f> vNormals; 
+		std::vector<Vec2f> vTextures;
 		
 		std::string line;
 
@@ -33,10 +37,38 @@ void CScene::ParseOBJ(const std::string& fileName)
 				vVertexes.emplace_back(100 * v);
 			}
 			else if (line == "f") {
-				Vec3i f;
-				for (int i = 0; i < 3; i++) ss >> f.val[i];
+				std::string aV, aN, aT, bV, bN, bT, cV, cN, cT;
+				getline(ss, aV, '/');
+				getline(ss, aT, '/');
+				getline(ss, aN, ' ');
+
+				getline(ss, bV, '/');
+				getline(ss, bT, '/');
+				getline(ss, bN, ' ');
+
+				getline(ss, cV, '/');
+				getline(ss, cT, '/');
+				getline(ss, cN, ' ');
+
 				// std::cout << "Face: " << f << std::endl;
-				Add(std::make_shared<CPrimTriangle>(vVertexes[f.val[0] - 1], vVertexes[f.val[1] - 1], vVertexes[f.val[2] - 1], pShader));
+				//Add(std::make_shared<CPrimTriangle>(vVertexes[f.val[0] - 1], vVertexes[f.val[1] - 1], vVertexes[f.val[2] - 1], pShader));
+
+				Add(std::make_shared<CPrimTriangle>(vVertexes[f.val[0] - 1], 
+				vVertexes[f.val[1] - 1], vVertexes[f.val[2] - 1], pShader));
+			}
+			else if (line == "vn") {
+				Vec3f vertexNormal;
+				for (int i = 0; i < 3; i++) ss >> vertexNormal.val[i];
+				vNormals.emplace_back(vertexNormal);
+			}
+			else if (line == "vt") {
+				Vec2f vt;
+				for (int i = 0; i < 2; i++) ss >> vt.val[i];
+				vTextures.emplace_back(vt);
+			}
+			else if (line == "#") {
+				std::string comment;
+				getline(ss, comment);
 			}
 			else {
 				std::cout << "Unknown key [" << line << "] met in the OBJ file" << std::endl;
